d44c53d73195dbd47823e9535b4c9a505a177e77
RomanVPX <dev@romanvpx.xyz>
2025-12-24 22:44:37 +0300

    Smoother animation, RGB order fix

--- keyboards/keychron/k13_max/ansi/rgb/keymaps/RomanVPX/life.c
+++ keyboards/keychron/k13_max/ansi/rgb/keymaps/RomanVPX/life.c
@@ -4,20 +4,21 @@
 #define LIFE_WIDTH GRID_WIDTH
 #define LIFE_HEIGHT GRID_HEIGHT
 #define LIFE_SPEED_MS 300
-#define LIFE_FADE_SUB 60
 
 #define LIFE_COLOR_ALIVE    RGB_SPRINGGREEN
-#define LIFE_COLOR_DYING    RGB_BLUE
+#define LIFE_COLOR_DYING    RGB_RED
 #define LIFE_COLOR_BG       10, 5, 8
+#define LIFE_FADE_SUB       2
 
-#define STATE_DEAD  0
-#define STATE_ALIVE 255
+#define DISPLAY_DEAD  0
+#define DISPLAY_ALIVE 255
 
 static bool life_active = false;
 static uint32_t life_timer = 0;
 static bool life_paused = false;
-static uint8_t life_grid[LIFE_HEIGHT][LIFE_WIDTH];
-static uint8_t life_next_grid[LIFE_HEIGHT][LIFE_WIDTH];
+static uint8_t life_display_grid[LIFE_HEIGHT][LIFE_WIDTH];
+static bool life_state_grid[LIFE_HEIGHT][LIFE_WIDTH];
+static bool life_next_state_grid[LIFE_HEIGHT][LIFE_WIDTH];
 
 bool life_is_active(void) {
     return life_active;
@@ -28,20 +29,28 @@ static inline void init_random_pattern(void) {
     for (uint8_t y = 0; y < LIFE_HEIGHT; y++) {
         for (uint8_t x = 0; x < LIFE_WIDTH; x++) {
             if (rand() % 3 == 0) { // 1/3 chance to be alive
-                life_grid[y][x] = STATE_ALIVE;
+                life_state_grid[y][x] = true;
+                life_display_grid[y][x] = DISPLAY_ALIVE;
+            } else {
+                life_state_grid[y][x] = false;
+                life_display_grid[y][x] = DISPLAY_DEAD;
             }
         }
     }
+}
+
+static inline RGB rgb_from_define(uint8_t r, uint8_t g, uint8_t b) {
+    return (RGB){ .r = r, .g = g, .b = b };
 }
 
 void life_game_start(void) {
     life_active = true;
     life_timer = timer_read();
 
-    // Clear grid
-    memset(life_grid, STATE_DEAD, sizeof(life_grid));
+    // Clear grids
+    memset(life_display_grid, DISPLAY_DEAD, sizeof(life_display_grid));
+    memset(life_state_grid, 0, sizeof(life_state_grid));
 
-    // Initialize with a random pattern
     init_random_pattern();
 }
 
@@ -61,7 +70,7 @@ static uint8_t count_neighbors(int8_t x, int8_t y) {
             int8_t nx = (x + dx + LIFE_WIDTH) % LIFE_WIDTH;
             int8_t ny = (y + dy + LIFE_HEIGHT) % LIFE_HEIGHT;
 
-            if (life_grid[ny][nx] == STATE_ALIVE) count++;
+            if (life_state_grid[ny][nx]) count++;
         }
     }
     return count;
@@ -71,25 +80,22 @@ static void life_update(void) {
     for (uint8_t y = 0; y < LIFE_HEIGHT; y++) {
         for (uint8_t x = 0; x < LIFE_WIDTH; x++) {
             uint8_t neighbors = count_neighbors(x, y);
-            uint8_t current_val = life_grid[y][x];
-            bool is_alive_now = (current_val == STATE_ALIVE);
+            bool is_alive = life_state_grid[y][x];
             bool will_be_alive = false;
 
-            if (is_alive_now) {
+            if (is_alive) {
+                // Survival: 2 or 3 neighbors
                 will_be_alive = (neighbors == 2 || neighbors == 3);
             } else {
+                // Birth: 3 neighbors
                 will_be_alive = (neighbors == 3);
             }
 
-            if (will_be_alive) {
-                life_next_grid[y][x] = STATE_ALIVE;
-            } else {
-                life_next_grid[y][x] = qsub8(current_val, LIFE_FADE_SUB);
-            }
+            life_next_state_grid[y][x] = will_be_alive;
         }
     }
 
-    memcpy(life_grid, life_next_grid, sizeof(life_grid));
+    memcpy(life_state_grid, life_next_state_grid, sizeof(life_state_grid));
 }
 
 void life_game_task(void) {
@@ -116,7 +122,7 @@ bool life_game_process_record(uint16_t keycode, keyrecord_t *record) {
         }
 
         if (keycode == KC_BSPC) {
-            memset(life_grid, STATE_DEAD, sizeof(life_grid));
+            memset(life_state_grid, 0, sizeof(life_state_grid));
             return false;
         }
 
@@ -128,12 +134,11 @@ bool life_game_process_record(uint16_t keycode, keyrecord_t *record) {
         for (uint8_t y = 0; y < LIFE_HEIGHT; y++) {
             for (uint8_t x = 0; x < LIFE_WIDTH; x++) {
                  if (pgm_read_byte(&GRID_MAP[y][x]) == matrix_idx) {
-                     if (life_grid[y][x] == STATE_ALIVE) {
-                         life_grid[y][x] = STATE_DEAD;
-                     } else {
-                         life_grid[y][x] = STATE_ALIVE;
-                     }
-                     return false; // Consume key - toggle cell state
+                     // Toggle state
+                     bool new_state = !life_state_grid[y][x];
+                     life_state_grid[y][x] = new_state;
+                     life_display_grid[y][x] = new_state ? DISPLAY_ALIVE : life_display_grid[y][x];
+                     return false; // Consume key
                  }
             }
         }
@@ -145,12 +150,12 @@ bool life_game_process_record(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-static inline RGB life_rgb_lerp(RGB a, RGB b, uint8_t frac) {
-    RGB res;
-    res.r = lerp8by8(a.r, b.r, frac);
-    res.g = lerp8by8(a.g, b.g, frac);
-    res.b = lerp8by8(a.b, b.b, frac);
-    return res;
+static inline RGB life_rgb_lerp(RGB* a, RGB* b, uint8_t frac) {
+    return (RGB) {
+        .r = lerp8by8(a->r, b->r, frac),
+        .g = lerp8by8(a->g, b->g, frac),
+        .b = lerp8by8(a->b, b->b, frac)
+    };
 }
 
 void life_game_render(void) {
@@ -161,17 +166,26 @@ void life_game_render(void) {
         rgb_matrix_set_color(i, 0, 0, 0);
     }
 
+    RGB bg_color = rgb_from_define(LIFE_COLOR_BG);
+    RGB target_color = rgb_from_define(LIFE_COLOR_DYING);
+
     for (uint8_t y = 0; y < LIFE_HEIGHT; y++) {
         for (uint8_t x = 0; x < LIFE_WIDTH; x++) {
+            if (life_state_grid[y][x]) {
+                life_display_grid[y][x] = DISPLAY_ALIVE;
+            } else {
+                life_display_grid[y][x] = qsub8(life_display_grid[y][x], LIFE_FADE_SUB);
+            }
+
             uint8_t m_idx = pgm_read_byte(&GRID_MAP[y][x]);
             if (m_idx != 0xFF) {
                 uint8_t l_idx = get_led_index_from_matrix(m_idx);
                 if (l_idx != NO_LED) {
-                    uint8_t val = life_grid[y][x];
-                    if (val == STATE_ALIVE) {
+                    uint8_t val = life_display_grid[y][x];
+                    if (val == DISPLAY_ALIVE) {
                         rgb_matrix_set_color(l_idx, LIFE_COLOR_ALIVE);
-                    } else if (val > STATE_DEAD) {
-                        RGB dying_color = life_rgb_lerp((RGB){LIFE_COLOR_BG}, (RGB){LIFE_COLOR_DYING}, val);
+                    } else if (val > DISPLAY_DEAD) {
+                        RGB dying_color = life_rgb_lerp(&bg_color, &target_color, val);
                         rgb_matrix_set_color(l_idx, dying_color.r, dying_color.g, dying_color.b);
                     } else {
                         rgb_matrix_set_color(l_idx, LIFE_COLOR_BG);
